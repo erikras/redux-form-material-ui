cb75b15ba26144784b967387d91f638f2a9770e4
Erik Rasmussen <rasmussenerik@gmail.com>
2016-12-01 18:52:07 +0100

    added Redbox back

--- example/src/index.js
+++ example/src/index.js
@@ -58,6 +58,22 @@ let render = () => {
 if (module.hot) {
   // Support hot reloading of components
   // and display an overlay for runtime errors
+  const renderApp = render
+  const renderError = (error) => {
+    console.error('ERROR:', error)  // eslint-disable-line no-console
+    const RedBox = require('redbox-react')
+    ReactDOM.render(
+      <RedBox error={error} className="redbox"/>,
+      dest
+    )
+  }
+  render = () => {
+    try {
+      renderApp()
+    } catch (error) {
+      renderError(error)
+    }
+  }
   const rerender = () => {
     setTimeout(render)
   }
