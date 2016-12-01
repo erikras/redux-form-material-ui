7757c7bf40247cf73860b7a7d8b9043fc13c3cbd
Erik Rasmussen <rasmussenerik@gmail.com>
2016-12-01 18:29:56 +0100

    dep update and timepicker fix

--- example/src/index.js
+++ example/src/index.js
@@ -58,21 +58,6 @@ let render = () => {
 if (module.hot) {
   // Support hot reloading of components
   // and display an overlay for runtime errors
-  const renderApp = render
-  const renderError = (error) => {
-    const RedBox = require('redbox-react')
-    ReactDOM.render(
-      <RedBox error={error} className="redbox"/>,
-      dest
-    )
-  }
-  render = () => {
-    try {
-      renderApp()
-    } catch (error) {
-      renderError(error)
-    }
-  }
   const rerender = () => {
     setTimeout(render)
   }
