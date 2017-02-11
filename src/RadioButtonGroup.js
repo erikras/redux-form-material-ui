b7b7b763d261470d6d4da6b56f9de2326ae3f679
xuewenfei <286xwf@163.com>
2017-02-11 13:39:05 +0800

    Update RadioButtonGroup.js
    fix RadioButtonGroup defaultSelected #84

--- src/RadioButtonGroup.js
+++ src/RadioButtonGroup.js
@@ -3,7 +3,11 @@ import createComponent from './createComponent'
 import mapError from './mapError'
 
 const mapValueToValueSelected = ({ input: { ...inputProps }, ...props }, errorProp) => {
-  return mapError({ ...props, input: { ...inputProps, valueSelected: inputProps.value } }, errorProp)
+  if(inputProps.value == '' && props.defaultSelected != undefined) {
+    return mapError({ ...props, input: { ...inputProps, valueSelected: props.defaultSelected } }, errorProp)
+  } else {
+    return mapError({ ...props, input: { ...inputProps, valueSelected: inputProps.value } }, errorProp)
+  }
 }
 
 export default createComponent(RadioButtonGroup, mapValueToValueSelected)
