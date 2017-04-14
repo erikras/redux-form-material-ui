5b7636e3b2391ece3cb25f8026e5a955f0b6cbf1
Erik Rasmussen <rasmussenerik@gmail.com>
2017-04-14 10:13:08 +0200

    Updated deps (#129)
    * Updated deps
    
    * fixed webpack.config.js
    
    * updated travis.yml
    
    * opened peer deps
    
    * dropped node v5

--- src/__tests__/Checkbox.spec.js
+++ src/__tests__/Checkbox.spec.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import TestUtils from 'react-addons-test-utils'
+import TestUtils from 'react-dom/test-utils'
 import expect from 'expect'
 import expectJsx from 'expect-jsx'
 import Checkbox from 'material-ui/Checkbox'
--- src/__tests__/Toggle.spec.js
+++ src/__tests__/Toggle.spec.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import TestUtils from 'react-addons-test-utils'
+import TestUtils from 'react-dom/test-utils'
 import expect from 'expect'
 import expectJsx from 'expect-jsx'
 import Toggle from 'material-ui/Toggle'
