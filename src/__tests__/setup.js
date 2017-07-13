1575549c396c990de9156cde311f5b0dac336339
Guilherme Pacheco <guilherme.f.pacheco@gmail.com>
2017-07-13 10:56:26 -0300

    Update dependencies

--- src/__tests__/setup.js
+++ src/__tests__/setup.js
@@ -1,5 +1,7 @@
-import { jsdom } from 'jsdom'
+import { JSDOM } from 'jsdom'
 
-global.document = jsdom('<!doctype html><html><body></body></html>')
-global.window = document.defaultView
-global.navigator = global.window.navigator
+const dom = new JSDOM('<!doctype html><html><body></body></html>')
+
+global.window = dom.window
+global.document = dom.window.document
+global.navigator = dom.window.navigator
