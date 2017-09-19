f228d5e6d8e84819f24e0a2803b915b7daa3d7e2
Guilherme Pacheco <guilherme.f.pacheco@gmail.com>
2017-09-19 02:10:08 -0300

    Export Select component from index file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export { default as Checkbox } from './Checkbox'
 export { default as RadioGroup } from './RadioGroup'
-export { default as TextField } from './TextField'
+export { default as Select } from './Select'
 export { default as Switch } from './Switch'
+export { default as TextField } from './TextField'
