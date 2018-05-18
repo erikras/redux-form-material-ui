7d48ebd18a198325dfc8cb08c858a79311563953
Travis Barney <travis.barney@lifeomic.com>
2018-05-17 22:45:59 -0600

    wip: Upgrade example dependencies, targeting webpack version 4.

--- example/webpack.config.dev.js
+++ example/webpack.config.dev.js
@@ -23,19 +23,19 @@ module.exports = {
     extensions: [ '.json', '.js' ]
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.jsx?/,
-        loaders: [ 'babel-loader', 'eslint-loader' ],
+        use: [ 'babel-loader', 'eslint-loader' ],
         include: path.join(__dirname, 'src')
       },
       {
         test: /\.json$/,
-        loader: 'json-loader'
+        use: 'json-loader'
       },
       {
         test: /\.md/,
-        loaders: [ "html-loader", "markdown-loader" ]
+        use: [ "html-loader", "markdown-loader" ]
       }
     ]
   }
